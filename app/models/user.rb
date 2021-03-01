4abf53224e42c9fad0368fdf55e07dbb56fc2d4f
Ralph <ralphoburu@gmail.com>
2021-03-01 14:33:31 +0300

    Fix sign up validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   validates :name, presence: true
   validates :email, confirmation: true
-  validates :email_confirmation, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
